e28975979ab010bbb77b792b583c99f4c42b199c
Bruno Chirelli <brunochirelli@gmail.com>
2024-12-21 06:54:18 +0000

    wrap App component in StylesProvider for class name generation

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 import Header from "./components/Header";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <Route exact path="/" component={MarketingApp} />
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <Header />
+        <Route exact path="/" component={MarketingApp} />
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
 
