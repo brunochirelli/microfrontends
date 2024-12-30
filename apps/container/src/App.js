976a0f64412cc71944bc7202dda331d3ce49cc3d
Bruno Chirelli <brunochirelli@gmail.com>
2024-12-30 06:21:11 +0000

    add GitHub Actions workflow for dashboard deployment; update routing in App component and implement redirect for signed-in users

--- apps/container/src/App.js
+++ apps/container/src/App.js
@@ -1,5 +1,6 @@
-import React, { lazy, Suspense, useState } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React, { lazy, Suspense, useEffect, useState } from "react";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
+import { createBrowserHistory } from "history";
 import {
   StylesProvider,
   createGenerateClassName,
@@ -14,11 +15,19 @@ const generateClassName = createGenerateClassName({
   productionPrefix: "co",
 });
 
+const history = createBrowserHistory();
+
 const App = () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
 
+  useEffect(() => {
+    if (isSignedIn) {
+      history.push("/dashboard");
+    }
+  }, [isSignedIn]);
+
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <StylesProvider generateClassName={generateClassName}>
         <Header
           isSignedIn={isSignedIn}
@@ -32,12 +41,15 @@ const App = () => {
                 onSignUp={() => setIsSignedIn(true)}
               />
             </Route>
-            <Route path="/dashboard" component={DashboardLazy} />
+            <Route path="/dashboard">
+              {!isSignedIn && <Redirect to="/" />}
+              <DashboardLazy />
+            </Route>
             <Route path="/" component={MarketingLazy} />
           </Switch>
         </Suspense>
       </StylesProvider>
-    </BrowserRouter>
+    </Router>
   );
 };
 
